106259c3d2a8fb11fac0b261004175d78a589f6d
Jason Barnett <jason.barnett@bwater.com>
2019-08-02 08:59:35 -0700

    Correct example

--- examples/ask_yes_no.sh
+++ examples/ask_yes_no.sh
@@ -4,7 +4,7 @@ source "${DIR}/helper.sh"
 
 answer=$(ask_yes_no "Are you going to work today?")
 if [[ $answer == true ]]; then
-  echo "How wonderful! Have a great day off."
-else
   echo "How sad :( -- try and make the best of it"
+else
+  echo "How wonderful! Have a great day off."
 fi
