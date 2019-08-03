62650bf8191060c0e4221e45813c974551e9a307
Jason Barnett <jason.barnett@bwater.com>
2019-08-03 09:27:13 -0700

    Use master_functions.sh in mysql-backup.sh

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -36,37 +36,7 @@ MYSQL_BACKUP_VERSION=1.0
 ## Functions ##
 ###############
 
-function ask_question {
-    question=$1
-    read -p "$question: "
-    echo $REPLY
-}
-
-function ask_yes_no {
-    question=$1
-    read -p "$question: [y/n] "
-    local answer=$(echo $REPLY | tr '[:upper:]' '[:lower:]')
-
-    while [[ "${answer}" != "yes" && "${answer}" != "no" && "${answer}" != "y" && "${answer}" != "n" ]];do
-        read -p "y/n only please... $question: [y/n] "
-        answer=$(echo $REPLY | tr '[:upper:]' '[:lower:]')
-    done
-
-    [[ "${answer}" == "yes" || "${answer}" == "y" ]] && echo true || echo false
-}
-
-function msg {
-    echo "$1"
-}
-
-function err_msg {
-    echo "$1" 1>&2
-}
-
-function fail_msg {
-    echo "$1" 1>&2
-    exit 1
-}
+. "$( dirname "${BASH_SOURCE[0]}" )/master_functions.sh"
 
 function update_config {
     local config_property config_value
