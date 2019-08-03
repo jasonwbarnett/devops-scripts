4a558d22621e6af8f16d89f20d025cdb8ef05bc1
Jason Barnett <jason.barnett@bwater.com>
2019-08-03 10:38:22 -0700

    Fix shellcheck reported issues

--- remove_dir_with_status.sh
+++ remove_dir_with_status.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-total_file_count=$(ls -1 | wc -l)
+total_file_count=$(find . -mindepth 1 -maxdepth 1 -not -path '*/\.*')
 num=1
 
-for dir in `ls -1`;do
+for dir in *;do
   percent=$(echo "scale=4; $num / $total_file_count * 100" | bc | sed 's|00$||g')
   echo "## Removing $dir ($num of $total_file_count) ${percent}%"
-  rm -Rf ./$dir
-  num=$(( $num + 1 ))
+  rm -Rf "./${dir}"
+  (( num++ ))
 done
